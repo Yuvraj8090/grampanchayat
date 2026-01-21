0dfeb474f0bd0980725b1c56769bbbfa897442bb
Yuvraj8090 <135937985+Yuvraj8090@users.noreply.github.com>
2026-01-21 16:38:04 +0530

    s

---
+++ grampradhan/database/migrations/2019_03_28_043644_create_addresses_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->index()->unsigned();
+            $table->string('name')->nullable();
+            $table->text('place')->nullable();
+            $table->string('distance')->nullable();
+            $table->text('address')->nullable();
+            $table->text('map')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('addresses');
+    }
+}
