b534d361b868468e5743dc0561953e0b66d58960
Yuvraj8090 <135937985+Yuvraj8090@users.noreply.github.com>
2026-01-22 15:37:26 +0530

    s

--- resources/views/errors/404.blade.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>404 - Gram Panchayat</title>
-</head>
-<body>
-	<center><img src="/images/404.png" class="img-responsive" alt="404 = Gram Panchayat"></center>
-</body>
-</html>
